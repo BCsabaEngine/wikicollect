26bec66e09bd128168a66f84974066c8e8b0e031
BCsabaEngine <csaba@balazs-magyar.hu>
2021-01-29 12:29:43 +0100

    mysql -> mysql2

--- index.js
+++ index.js
@@ -1,61 +1,5 @@
-const Database = require('./lib/Database')
+const { StoreWikiPage, FindUnProcessed, GetPages } = require('./lib/database')
 const { GetWikiPage } = require('./lib/Wiki')
-
-let dbinstance = null
-let dbuse = 0
-function CreateDatabase() {
-  if (dbuse > 49) {
-    dbinstance.close()
-    dbinstance = null
-  }
-
-  if (!dbinstance) {
-    dbinstance = new Database({
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'wikicollect'
-    });
-    dbuse = 0
-  }
-
-  dbuse++
-  return dbinstance
-}
-
-async function StoreWikiPage(pageinfo) {
-  const database = CreateDatabase();
-
-  await database.query('SELECT 1 FROM Page WHERE Url = ?', pageinfo.url)
-    .then((exists) => {
-      if (exists && exists.length) return
-
-      var pagerow = { Url: pageinfo.url, Title: pageinfo.title, Content: pageinfo.text };
-      database.query('INSERT INTO Page SET ?', pagerow)
-    })
-
-
-  await database.query('UPDATE Url SET Processed = NOW() WHERE Url = ?', pageinfo.url)
-
-  for (const url of pageinfo.urls)
-    await database.query('SELECT 1 FROM Url WHERE Url = ?', url)
-      .then((exists) => {
-        if (exists && exists.length) return
-
-        var urlrow = { Url: url };
-        database.query('INSERT INTO Url SET ?', urlrow)
-      })
-}
-
-async function FindUnProcessed() {
-  const database = CreateDatabase();
-
-  const result = await database.query('SELECT Url FROM Url WHERE Processed IS NULL ORDER BY RAND() LIMIT 1')
-  if (result && result.length)
-    return result[0].Url
-
-  return null
-}
 
 async function StartAt(startpage) {
   const pageinfo = await GetWikiPage(startpage);
@@ -77,9 +21,30 @@ async function RunUnprocessed() {
   }
 }
 
+async function FillMemIndex() {
+  const pages = await GetPages(1)
+  for (const page of pages) {
+    const content = page.Content.replaceAll('\t', '').replaceAll('\n\n', '\n')
+
+    const lines = content.split('\n')
+    lines.push(page.Title)
+
+    const sentences = []
+    for (const line of lines) {
+      const lsents = line.split('. ')
+      for (const lsent of lsents)
+        if (lsent.trim())
+          sentences.push(lsent.trim())
+    }
+
+    console.log(sentences)
+  }
+}
+
 try {
   //StartAt('/wiki/Pécs')
-  RunUnprocessed()
+  //RunUnprocessed()
+  FillMemIndex()
 }
 catch (error) {
   console.log(error.message)
